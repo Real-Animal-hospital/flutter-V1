87c20cb609f56545bde5e9868e3e4f47edb1e29b
sinbee0402 <osb3476@gmail.com>
2023-08-24 20:03:32 +0900

    Feat: login api 수정

--- lib/data/data_source/login_api.dart
+++ lib/data/data_source/login_api.dart
@@ -5,11 +5,10 @@ import 'package:http/http.dart' as http;
 import 'package:animal_hospital/domain/model/login/login_request.dart';
 
 class LoginApi {
-  static const baseURL = 'https://34.168.248.106/swagger/?format=openapi';
+  static const baseURL = 'https://34.168.248.106';
 
   Future<LoginRequest> getLogin() async {
-    final response = await http
-        .get(Uri.parse('$baseURL#/definitions/LoginRequestLoginRequest'));
+    final response = await http.get(Uri.parse('$baseURL/login'));
 
     return LoginRequest.fromJson(jsonDecode(response.body));
   }
