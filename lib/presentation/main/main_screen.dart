934968c30eae17b737d4f53d761f0827fdd6590c
sinbee0402 <osb3476@gmail.com>
2023-08-24 22:12:49 +0900

    feat: bottom_navigation_widget components 작성

--- lib/presentation/main/main_screen.dart
+++ lib/presentation/main/main_screen.dart
@@ -1,10 +1,44 @@
+import 'dart:ui';
+
+import 'package:animal_hospital/presentation/components/bottom_navigation_widget.dart';
 import 'package:flutter/material.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('견종 선택'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.share),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.settings),
+          )
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationWidget(
+        onItemTapped: _onItemTapped,
+        currentIndex: _selectedIndex,
+      ),
+    );
   }
 }
