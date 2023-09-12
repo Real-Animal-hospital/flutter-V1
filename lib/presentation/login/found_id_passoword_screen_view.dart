882e82a5a9b8bd8c136585b0a0a91ec569982581
solinGIL <rhkrvkfk@hotmail.com>
2023-09-12 14:10:31 +0900

    refactor: 아이디찾기 화면 추가했으나, 수정 필요: 간격 조절, 색 조절

--- lib/presentation/login/found_id_passoword_screen_view.dart
+++ lib/presentation/login/found_id_passoword_screen_view.dart
@@ -8,90 +8,131 @@ class FoundIdPassword extends StatefulWidget {
   State<FoundIdPassword> createState() => _FoundIdPasswordState();
 }
 
+// TODO: 아이디/비밀번호 찾기 간격 조절
 class _FoundIdPasswordState extends State<FoundIdPassword> {
+  bool isPasswordRecovery = false;
+
   @override
   Widget build(BuildContext context) {
     // 가로 회전 안되게 하는 코드
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-    final screenHeight = MediaQuery.of(context).size.height;
-    final appBarHeight = AppBar().preferredSize.height;
-    final availableHeight = screenHeight - appBarHeight - MediaQuery.of(context).padding.top;
 
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-          icon: Icon(Icons.arrow_back),
+          icon: Icon(Icons.arrow_back, color: Colors.black),
           onPressed: () {
             Navigator.pop(context);
           },
         ),
-        title: Text("아이디/비밀번호 찾기"),
+        title: Text("아이디/비밀번호 찾기", style: TextStyle(color: Colors.black)),
         backgroundColor: Colors.transparent,
+        elevation: 0,
       ),
-      body: SingleChildScrollView(
-        child: ConstrainedBox(
-          constraints: BoxConstraints(
-            minHeight: availableHeight,
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 30),
+              child: Center(
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isPasswordRecovery = false;
+                        });
+                      },
+                      child: Text(
+                        '메일아이디 찾기',
+                        style: TextStyle(
+                          color: isPasswordRecovery
+                              ? Color(0xFF191C1B)
+                              : Color(0xFF43D9C0),
+                          fontSize: 16,
+                          fontFamily: 'Pretendard',
+                          fontWeight: FontWeight.w400,
+                          height: 1.50,
+                          letterSpacing: 0.50,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 100),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isPasswordRecovery = true;
+                        });
+                      },
+                      child: Text(
+                        '비밀번호 찾기',
+                        style: TextStyle(
+                          color: isPasswordRecovery
+                              ? Color(0xFF43D9C0)
+                              : Color(0xFF191C1B),
+                          fontSize: 16,
+                          fontFamily: 'Pretendard',
+                          fontWeight: FontWeight.w400,
+                          height: 1.50,
+                          letterSpacing: 0.50,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 35),
+              width: 320,
+              child: Column(
                 children: [
-                  SizedBox(height: availableHeight * 0.05),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      GestureDetector(
-                        onTap: () {},
-                        child: Text('메일아이디 찾기'),
-                      ),
-                      GestureDetector(
-                        onTap: () {},
-                        child: Text('비밀번호 찾기'),
-                      ),
-                    ],
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText:
+                          isPasswordRecovery ? '이름을 입력해주세요' : '이메일을 입력해주세요',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                  SizedBox(height: availableHeight * 0.05),
-                  Container(
-                    width: 320,
-                    child: TextField(
+                  if (isPasswordRecovery) ...[
+                    SizedBox(height: 10),
+                    TextField(
                       decoration: InputDecoration(
-                        hintText: '이메일을 입력해주세요',
+                        hintText: '휴대폰 번호를 입력해주세요',
                         border: OutlineInputBorder(),
                       ),
                     ),
-                  ),
+                  ]
                 ],
               ),
-              Column(
-                children: [
-                  Container(
-                    width: 280,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(13),
-                      border: Border.all(color: Color(0xFF43D9C0)),
-                      color: Color(0xFF43D9C0),
-                    ),
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.transparent,
-                        elevation: 0,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      child: Text('확인'),
-                    ),
-                  ),
-                  SizedBox(height: availableHeight * 0.05),
-                  Text("@2023 Dogdoc All copyrights reserved"),
-                ],
+            ),
+            SizedBox(height: 100),
+            Container(
+              width: 280,
+              height: 50,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(13),
+                border: Border.all(color: Color(0xFF43D9C0)),
+                color: Color(0xFF43D9C0),
               ),
-            ],
-          ),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.transparent,
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Text('확인'),
+              ),
+            ),
+            SizedBox(height: 30),
+            Text("@2023 Dogdoc All copyrights reserved"),
+          ],
         ),
       ),
     );
