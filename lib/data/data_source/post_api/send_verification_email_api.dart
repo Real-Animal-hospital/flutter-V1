a98132adb68e1713df6629a01d98651f1db8b2dd
solinGIL <rhkrvkfk@hotmail.com>
2023-09-18 04:36:04 +0900

    feat: 이메일 인증 로직 수정

--- lib/data/data_source/post_api/send_verification_email_api.dart
+++ lib/data/data_source/post_api/send_verification_email_api.dart
@@ -1,17 +1,19 @@
+import 'dart:convert';
+
 import 'package:animal_hospital/data/data_source/custom_api_service.dart';
 
 class SendVerificationEmail {
-  Future<void> sendEmail(String email, {String? password}) async {
-    final apiService = ApiService(
-        baseUrl: 'https://dogdr.duckdns.org/send_verification_email/');
+  Future<void> sendEmail(String email, String password) async {
+    final apiService = ApiService(baseUrl: 'https://dogdr.duckdns.org');
+    // print(apiService.baseUrl);
 
-    final payload = {
-      "email": "string",
-      if (password != null) "password": password,
-    };
+    final requestBody = {"user_id": email, "password": password};
+    print('requestBody: $requestBody');
+
     try {
-      final data = await apiService.customPostAPI('인증을 보냈습니다.', payload);
-      print(data);
+      final data = await apiService.customPostAPI(
+          'send_verification_email/', requestBody);
+      print('텍스트컨트롤러에서 나온 데이터:${data}');
     } catch (e) {
       print('데이터를 가져오지 못했씁니다.: $e');
     }
