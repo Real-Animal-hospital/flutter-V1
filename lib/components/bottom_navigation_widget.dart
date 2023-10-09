cfc1e4428d56d55a31fc4bf85c6c8fa7dc182238
solin <rhkrvkfk@hotmail.com>
2023-10-10 02:21:24 +0900

    feat: 바텀네비게이션 피그마 디자인대로 변경

--- lib/components/bottom_navigation_widget.dart
+++ lib/components/bottom_navigation_widget.dart
@@ -1,50 +1,74 @@
+import 'package:animal_hospital/home/dogdrHome/presentation/home_screen_view.dart';
 import 'package:flutter/material.dart';
 
-class BottomNavigationWidget extends StatefulWidget {
-  final ValueChanged<int> onItemTapped;
-  final int currentIndex;
-
-  const BottomNavigationWidget({
-    Key? key,
-    required this.onItemTapped,
-    required this.currentIndex,
-  }) : super(key: key);
+class DogDrBottomNavigationWidget extends StatefulWidget {
+  const DogDrBottomNavigationWidget({super.key});
 
   @override
-  State<BottomNavigationWidget> createState() => _BottomNavigationWidgetState();
+  State<DogDrBottomNavigationWidget> createState() =>
+      _DogDrBottomNavigationWidgetState();
 }
 
-class _BottomNavigationWidgetState extends State<BottomNavigationWidget> {
+class _DogDrBottomNavigationWidgetState
+    extends State<DogDrBottomNavigationWidget> {
+  int _currentIndex = 0;
+
+  final _pages = const [
+    DogDrHomeScreen(),
+    DogDrHomeScreen(),
+    DogDrHomeScreen(),
+    DogDrHomeScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      items: const [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.local_hospital_outlined),
-          activeIcon: Icon(Icons.local_hospital),
-          label: '증상찾기',
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 0.0,
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_filled),
+            activeIcon: Icon(Icons.home_filled),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            activeIcon: Icon(Icons.search),
+            label: '병원검색',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bookmark),
+            activeIcon: Icon(Icons.bookmark),
+            label: '보관함',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            activeIcon: Icon(Icons.account_circle),
+            label: '프로필',
+          ),
+        ],
+        selectedItemColor: const Color(0xFF43D9C0),
+        unselectedItemColor: const Color(0xFF414941),
+        selectedLabelStyle: const TextStyle(
+          color: Color(0xFF323232),
+          fontSize: 13,
+          fontFamily: 'Pretendard',
+          fontWeight: FontWeight.w500,
+          letterSpacing: 0.10,
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.search),
-          activeIcon: Icon(Icons.search),
-          label: '병원검색',
+        // 선택된 아이템 레이블 스타일
+        unselectedLabelStyle: const TextStyle(
+          color: Color(0xFF323232),
+          fontSize: 13,
+          fontFamily: 'Pretendard',
+          fontWeight: FontWeight.w500,
+          letterSpacing: 0.10,
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.bookmark_border),
-          activeIcon: Icon(Icons.bookmark),
-          label: '북마크',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.person_outline),
-          activeIcon: Icon(Icons.person),
-          label: '증상찾기',
-        ),
-      ],
-      selectedItemColor: const Color(0xFF608BFF),
-      unselectedItemColor: const Color(0xFF414941),
-      currentIndex: widget.currentIndex,
-      onTap: widget.onItemTapped,
-      type: BottomNavigationBarType.fixed,
+        type: BottomNavigationBarType.fixed,
+      ),
     );
   }
 }
