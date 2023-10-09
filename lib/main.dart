fa6705e4656eff8b24ef3b36cf48143fb14a7234
solin <rhkrvkfk@hotmail.com>
2023-10-10 02:24:27 +0900

    feat: 앱 최초 실행 후 인트로 화면 또 안뜨게 구현. (테스트 아직 안함)

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,39 @@
+import 'package:animal_hospital/components/bottom_navigation_widget.dart';
 import 'package:animal_hospital/core/router.dart';
-import 'package:animal_hospital/presentation/main/main_screen.dart';
+import 'package:animal_hospital/intro_view/splash/presentation/splash_first_screen_view.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // 앱 실행 전 초기화
+
+  final prefs = await SharedPreferences.getInstance();
+  final bool isFirstTime = prefs.getBool('isFirstTime') ?? true;
+
+  if (isFirstTime) {
+    await prefs.setBool('isFirstTime', false);
+  }
+
+  runApp(MyApp(isFirstTime: isFirstTime));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final bool isFirstTime;
+
+  const MyApp({super.key, required this.isFirstTime});
 
   @override
   Widget build(BuildContext context) {
+    if (isFirstTime) {
+      router.go('/intro');
+    } else {
+      router.go('/');
+    }
+
     return MaterialApp.router(
       routerConfig: router,
       theme: ThemeData(
-       colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
       ),
     );
