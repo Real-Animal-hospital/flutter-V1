ced8d9249102c3f9ef9b4f15ca0fbe48489540ac
solin <rhkrvkfk@hotmail.com>
2023-10-10 02:22:39 +0900

    feat: 라우터 경로 추가

--- lib/core/router.dart
+++ lib/core/router.dart
@@ -1,6 +1,9 @@
 import 'package:animal_hospital/auth/login/presentation/found_id_passoword_screen_view.dart';
 import 'package:animal_hospital/auth/login/presentation/login_screen_view.dart';
 import 'package:animal_hospital/auth/siginup/presentation/email_signup_screen_view.dart';
+import 'package:animal_hospital/components/bottom_navigation_widget.dart';
+import 'package:animal_hospital/intro_view/introduce/presentation/introduce_screen_view.dart';
+import 'package:animal_hospital/intro_view/splash/presentation/splash_first_screen_view.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -9,7 +12,7 @@ final GoRouter router = GoRouter(
     GoRoute(
       path: '/',
       builder: (BuildContext context, GoRouterState state) {
-        return const LoginScreen();
+        return const SplashFirstScreen();
       },
     ),
     GoRoute(
@@ -22,6 +25,24 @@ final GoRouter router = GoRouter(
       path: '/foundid',
       builder: (BuildContext context, GoRouterState state) {
         return const FoundIdPassword();
+      },
+    ),
+    GoRoute(
+      path: '/login',
+      builder: (BuildContext context, GoRouterState state) {
+        return const LoginScreen();
+      },
+    ),
+    GoRoute(
+      path: '/home',
+      builder: (BuildContext context, GoRouterState state) {
+        return const DogDrBottomNavigationWidget();
+      },
+    ),
+    GoRoute(
+      path: '/intro',
+      builder: (BuildContext context, GoRouterState state) {
+        return const IntroduceScreenView();
       },
     ),
   ],
