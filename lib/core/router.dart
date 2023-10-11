d28c04c5e99aa62b2ef754d2361a38f05cb12d79
solin <rhkrvkfk@hotmail.com>
2023-10-11 22:22:36 +0900

    라우터: 뉴스 화면 경로 추가 펍스펙: assets 추가

--- lib/core/router.dart
+++ lib/core/router.dart
@@ -2,6 +2,7 @@ import 'package:animal_hospital/auth/login/presentation/found_id_passoword_scree
 import 'package:animal_hospital/auth/login/presentation/login_screen_view.dart';
 import 'package:animal_hospital/auth/siginup/presentation/email_signup_screen_view.dart';
 import 'package:animal_hospital/components/bottom_navigation_widget.dart';
+import 'package:animal_hospital/home/dogdrNews/presentation/news_screen_view.dart';
 import 'package:animal_hospital/intro_view/introduce/presentation/introduce_screen_view.dart';
 import 'package:animal_hospital/intro_view/splash/presentation/splash_first_screen_view.dart';
 import 'package:flutter/material.dart';
@@ -43,6 +44,12 @@ final GoRouter router = GoRouter(
       path: '/intro',
       builder: (BuildContext context, GoRouterState state) {
         return const IntroduceScreenView();
+      },
+    ),
+    GoRoute(
+      path: '/news',
+      builder: (BuildContext context, GoRouterState state) {
+        return const NewsScreenView();
       },
     ),
   ],
