4c8e78ab27eb488e794a788e706496fcf0846d20
solin <rhkrvkfk@hotmail.com>
2023-11-05 08:30:07 +0900

    fix: const 붙임, 독닥 시작하기 폰트 색 하얀색으로 변경, 독닥 시작하기 누르면 로그인 화면으로 넘어가도록 변경

--- lib/auth/siginup/presentation/signup_done_screen_view.dart
+++ lib/auth/siginup/presentation/signup_done_screen_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:go_router/go_router.dart';
 
 class SignUpDoneScreen extends StatefulWidget {
   const SignUpDoneScreen({Key? key}) : super(key: key);
@@ -18,8 +19,8 @@ class _SignUpDoneScreen extends State<SignUpDoneScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Spacer(flex: 2),
-            Text(
+            const Spacer(flex: 2),
+            const Text(
               '가입이 완료되었습니다!!',
               style: TextStyle(
                 fontSize: 17,
@@ -27,18 +28,19 @@ class _SignUpDoneScreen extends State<SignUpDoneScreen> {
                 color: Color(0xFF000000),
               ),
             ),
-            SizedBox(height: 35),
+            const SizedBox(height: 35),
             Container(
               width: 280,
               height: 50,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(13),
-                border: Border.all(color: Color(0xFF43D9C0)),
-                color: Color(0xFF43D9C0),
+                border: Border.all(color: const Color(0xFF43D9C0)),
+                color: const Color(0xFF43D9C0),
               ),
               child: ElevatedButton(
                 onPressed: () {
                   // 회원가입 버튼이 눌렸을 때 실행할 동작
+                  context.push('/login');
                 },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.transparent, // 배경색을 투명하게 설정
@@ -47,28 +49,29 @@ class _SignUpDoneScreen extends State<SignUpDoneScreen> {
                     borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-                child: Text(
+                child: const Text(
                   '독닥 시작하기',
                   style: TextStyle(
+                    color: Colors.white,
                     fontSize: 16,
                     fontWeight: FontWeight.w500,
                   ),
                 ),
               ),
             ),
-            Expanded(
+            const Expanded(
                 child: SizedBox(
               height: 50,
             )),
             Container(
-                child: Text(
+                child: const Text(
               "@2023 Dogdoc All copyrights reserved",
               style: TextStyle(
                 fontSize: 13.5,
                 fontWeight: FontWeight.w400, // 오타 수정
               ),
             )),
-            Expanded(
+            const Expanded(
                 child: SizedBox(
               height: 30,
             )),
@@ -81,7 +84,7 @@ class _SignUpDoneScreen extends State<SignUpDoneScreen> {
 }
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     home: SignUpDoneScreen(),
   ));
 }
