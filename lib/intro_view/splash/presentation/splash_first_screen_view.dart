63266d4557752c5edcc0f045cded10e6fd237c3c
solin <rhkrvkfk@hotmail.com>
2023-11-17 15:04:33 +0900

    refactor: 스플래시 화면 수정

--- lib/intro_view/splash/presentation/splash_first_screen_view.dart
+++ lib/intro_view/splash/presentation/splash_first_screen_view.dart
@@ -1,4 +1,4 @@
-import 'package:animal_hospital/core/router.dart';
+ import 'package:animal_hospital/core/router.dart';
 import 'package:animal_hospital/intro_view/introduce/presentation/introduce_screen_view.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -23,38 +23,23 @@ class _SplashFirstScreenState extends State<SplashFirstScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFF43D9C0),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             const Spacer(flex: 2),
-            Container(
-              width: 148,
-              height: 148,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: const Color(0xff43d9c0),
-              ),
+            Image.asset(
+              'assets/logo_icon/독닥_아이콘_231011(2)1.png', // ll.png 이미지
             ),
-            const Spacer(flex: 2), // 간격 추가
-            Container(
-              width: MediaQuery.of(context).size.width * 0.85,
-              child: const Divider(
-                // 경계선 추가
-                color: Colors.grey,
-                thickness: 1,
-              ),
+            const SizedBox(height: 5.0),
+            Image.asset(
+              'assets/logo_icon/독닥_로고_흰색_231115.png',
+              width: 200.0,
+              height: 50.0,
             ),
-            const SizedBox(height: 30),
-            const Text(
-              "@2023 Dogdoc All copyrights reserved",
-              style: TextStyle(
-                fontSize: 12,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-            const Spacer(flex: 1),
+            const Spacer(flex: 2),
           ],
         ),
       ),
